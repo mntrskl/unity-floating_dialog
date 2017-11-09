23d46a676128f8db600fd9643519e48c3112a972
Diego <diego.89@gmail.com>
2017-11-09 00:24:15 -0300

    Project Init

---
+++ Assets/Scripts/Clicked.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicked : MonoBehaviour {
+
+    private bool displayed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (displayed)
+        {
+            this.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Close");
+        } else
+        {
+            this.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Open");
+        }
+        displayed = !displayed;
+    }
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public float time = 3f;
+    public AnimationCurve curve;
+
+    Camera cameraToLookAt;
+
+    // Use this for initialization 
+    void Start()
+    {
+        cameraToLookAt = Camera.main;
+
+    }
+
+    // Update is called once per frame 
+    void LateUpdate()
+    {
+        Transform from = transform;
+        Transform to = cameraToLookAt.transform;
+        //transform.LookAt(cameraToLookAt.transform);
+        //transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+        //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, Time.time * speed);
+        transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, curve.Evaluate(speed/4));
+    }
+}
